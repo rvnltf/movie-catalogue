ce984dd23d8914780e62528c6b850433486f2a35
rvnltf <aldirivan56@gmail.com>
2021-06-12 14:19:11 +0700

    Creating API Content

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,15 +1,19 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
+import { createMovieDetailTemplate } from "../templates/template-creator";
 
 const Detail = {
   async render() {
-    return `<h2>Details Page</h2>`;
+    return `
+    <div id="movie" class="movie"></div>
+    `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movies = await TheMovieDbSource.detailMovies(url.id);
-    console.log(movies);
+    const movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML = createMovieDetailTemplate(movies);
   },
 };
 
