11eae398d8ff404a2a250e4cae82180535f86abf
rvnltf <aldirivan56@gmail.com>
2021-06-14 20:06:20 +0700

    Creating Like Button

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -35,5 +35,21 @@ const createMovieItemTemplate = (movie) => `
     </div>
   </div>
 `;
+const createLikeButtonTemplate = () => `
+<button aria-label="like this movie" id="likeButton" class="like">
+   <i class="fa fa-heart-o" aria-hidden="true"></i>
+</button>
+`;
 
-export { createMovieDetailTemplate, createMovieItemTemplate };
+const createLikedButtonTemplate = () => `
+<button aria-label="unlike this movie" id="likeButton" class="like">
+  <i class="fa fa-heart" aria-hidden="true"></i>
+</button>
+`;
+
+export {
+  createMovieDetailTemplate,
+  createMovieItemTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+};
