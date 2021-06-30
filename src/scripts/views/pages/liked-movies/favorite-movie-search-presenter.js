7c88092ccaebfca315cfd7ae978146b6ec3b1a31
rvnltf <aldirivan56@gmail.com>
2021-06-30 22:01:53 +0700

    When query is not empty test

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -12,8 +12,15 @@ class FavoriteMovieSearchPresenter {
   }
 
   async _searchMovies(latestQuery) {
-    this._latestQuery = latestQuery;
-    const foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    this._latestQuery = latestQuery.trim();
+
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
+
     if (foundMovies) {
       this._showFoundMovies(foundMovies);
     }
