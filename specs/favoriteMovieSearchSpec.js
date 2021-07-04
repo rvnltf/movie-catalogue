480a50185419c1e0e7e76ea60adb0e110030564a
rvnltf <aldirivan56@gmail.com>
2021-07-04 08:44:55 +0700

    code summary

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -1,6 +1,8 @@
-import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
+import FavoriteMovieSearchPresenter
+  from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
-import FavoriteMovieSearchView from '../src/scripts/views/pages/liked-movies/favorite-movie-search-view';
+import FavoriteMovieSearchView
+  from '../src/scripts/views/pages/liked-movies/favorite-movie-search-view';
 
 describe('Searching movies', () => {
   let presenter;
@@ -72,98 +74,17 @@ describe('Searching movies', () => {
         .toEqual('Satu');
     });
 
-    it('should show the title of the found movies', () => {
-      presenter._showFoundMovies([{
-        id: 1,
-        title: 'Satu',
-      }]);
-      expect(document.querySelectorAll('.movie__title')
-        .item(0).textContent)
-        .toEqual('Satu');
+    it('should show - when the movie returned does not contain a title', (done) => {
+      document.getElementById('movie-search-container').addEventListener('movies:searched:updated', () => {
+        const movieTitles = document.querySelectorAll('.movie__title');
+        expect(movieTitles.item(0).textContent).toEqual('-');
 
-      presenter._showFoundMovies(
-        [{
-          id: 1,
-          title: 'Satu',
-        }, {
-          id: 2,
-          title: 'Dua',
-        }],
-      );
+        done();
+      });
 
-      const movieTitles = document.querySelectorAll('.movie__title');
-      expect(movieTitles.item(0).textContent)
-        .toEqual('Satu');
-      expect(movieTitles.item(1).textContent)
-        .toEqual('Dua');
-    });
-
-    it('should show - for found movie without title', () => {
-      presenter._showFoundMovies([{ id: 1 }]);
-
-      expect(document.querySelectorAll('.movie__title')
-        .item(0).textContent)
-        .toEqual('-');
-    });
-
-    it('should show the movies found by Favorite Movies', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
-          expect(document.querySelectorAll('.movie').length)
-            .toEqual(3);
-          done();
-        });
-
-      favoriteMovies.searchMovies.withArgs('film a')
-        .and
-        .returnValues([
-          {
-            id: 111,
-            title: 'film abc',
-          },
-          {
-            id: 222,
-            title: 'ada juga film abcde',
-          },
-          {
-            id: 333,
-            title: 'ini juga boleh film a',
-          },
-        ]);
-
-      searchMovies('film a');
-    });
-
-    it('should show the name of the movies found by Favorite Movies', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
-          const movieTitles = document.querySelectorAll('.movie__title');
-          expect(movieTitles.item(0).textContent)
-            .toEqual('film abc');
-          expect(movieTitles.item(1).textContent)
-            .toEqual('ada juga film abcde');
-          expect(movieTitles.item(2).textContent)
-            .toEqual('ini juga boleh film a');
-
-          done();
-        });
-
-      favoriteMovies.searchMovies.withArgs('film a')
-        .and
-        .returnValues([
-          {
-            id: 111,
-            title: 'film abc',
-          },
-          {
-            id: 222,
-            title: 'ada juga film abcde',
-          },
-          {
-            id: 333,
-            title: 'ini juga boleh film a',
-          },
-        ]);
+      favoriteMovies.searchMovies.withArgs('film a').and.returnValues([
+        { id: 444 },
+      ]);
 
       searchMovies('film a');
     });
@@ -194,6 +115,9 @@ describe('Searching movies', () => {
       expect(favoriteMovies.getAllMovies)
         .toHaveBeenCalled();
     });
+  });
+
+  describe('When no favorite movies could be found', () => {
     it('should show the empty message', (done) => {
       document.getElementById('movie-search-container')
         .addEventListener('movies:searched:updated', () => {
@@ -202,7 +126,24 @@ describe('Searching movies', () => {
           done();
         });
 
-      favoriteMovies.searchMovies.withArgs('film a').and.returnValues([]);
+      favoriteMovies.searchMovies.withArgs('film a')
+        .and
+        .returnValues([]);
+
+      searchMovies('film a');
+    });
+
+    it('should not show any movie', (done) => {
+      document.getElementById('movie-search-container')
+        .addEventListener('movies:searched:updated', () => {
+          expect(document.querySelectorAll('.movie').length)
+            .toEqual(0);
+          done();
+        });
+
+      favoriteMovies.searchMovies.withArgs('film a')
+        .and
+        .returnValues([]);
 
       searchMovies('film a');
     });
