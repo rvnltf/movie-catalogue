2e0d08c82fc86913897d966ea662b1c779710280
rvnltf <aldirivan56@gmail.com>
2021-07-04 13:59:42 +0700

    Codecept Init

---
+++ e2e/Liking_Movies.spec.js
@@ -0,0 +1,14 @@
+Feature('Liking Movies');
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+Scenario('showing empty liked movies', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+});
+Scenario('liking one movie', ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+  // … kita akan mengisi uji coba berikutnya …
+});
